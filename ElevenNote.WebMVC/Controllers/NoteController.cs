5dcf2d3e720e4bdb5c2d3d75e246d54c70d6249a
Quese22 <MarqueseMhayes@gmail.com>
2019-10-10 17:14:38 -0400

    adding the details method

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -51,6 +51,16 @@ namespace ElevenNote.WebMVC.Controllers
             var model = svc.GetNoteById(id);
 
             return View(model);
+
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var service = CreateNoteService();
+            var detail = service.GetNoteById(id);
+            var model = new NoteEdit { NoteId = detail.NoteId, Title = detail.Title, Content = detail.Content };
+
+            return View(model);
         }
 
         private NoteService CreateNoteService()
