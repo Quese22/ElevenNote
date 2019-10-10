ba65318cd320bb8ecb2955104c572623987985f0
Quese22 <MarqueseMhayes@gmail.com>
2019-10-10 16:52:06 -0400

    update

--- ElevenNote.Services/NoteService.cs
+++ ElevenNote.Services/NoteService.cs
@@ -45,5 +45,24 @@ namespace ElevenNote.Services
             }
         }
 
+        public NoteDetail GetNoteById(int id) //using statement to connect to the database///
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity = ctx
+                    .Notes
+                    .Single(e => e.NoteId == id && e.OwnerId == _userId);
+                return
+                    new NoteDetail
+                    {
+                        NoteId = entity.NoteId,
+                        Title = entity.Title,
+                        Content = entity.Content,
+                        CreatedUtc = entity.CreatedUtc,
+                        ModifiedUtc = entity.ModifiedUtc,
+                    };
+            }
+        }
+
     }
 }
